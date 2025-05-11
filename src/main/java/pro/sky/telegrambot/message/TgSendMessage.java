b54de3c4f9a539f5c3218767ce19da11fd5b41fd
pavel <pavelkoch7@yandex.ru>
2025-05-11 21:18:29 +0300

    corrected version2

--- src/main/java/pro/sky/telegrambot/message/TgSendMessage.java
+++ src/main/java/pro/sky/telegrambot/message/TgSendMessage.java
@@ -5,11 +5,18 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import pro.sky.telegrambot.listener.TelegramBotUpdatesListener;
 
+@Component
 public class TgSendMessage {
-    TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    public TgSendMessage(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
     public void sendMessage(Long chatId, String message) {
         SendMessage sendMessage = new SendMessage(chatId, message);
         SendResponse sendResponse = telegramBot.execute(sendMessage);
